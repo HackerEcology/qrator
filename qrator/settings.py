122953dc5e12a1fe12a7bc490c2b24aa2e3e962e
arcolife <arcshams@gmail.com>
2014-08-29 04:20:35 +0530

    deactivate JsonWriterPipeline

--- qrator/settings.py
+++ qrator/settings.py
@@ -15,17 +15,7 @@ NEWSPIDER_MODULE = 'qrator.spiders'
 #USER_AGENT = 'qrator (+http://www.yourdomain.com)'
 
 ITEM_PIPELINES = {
-    'qrator.pipelines.JsonWriterPipeline': 10,
+    # 'qrator.pipelines.JsonWriterPipeline': 10,
     'qrator.pipelines.FilterHTMLPipeline': 11,
     'qrator.pipelines.ElasticSearchPipeline': 12,
-    #'scrapyelasticsearch.ElasticSearchPipeline',
 }
-
-### DONT UNCOMMENT: It was faulty! 
-# ELASTICSEARCH_SERVER = 'localhost' # If not 'localhost' prepend 'http://'
-# ELASTICSEARCH_PORT = 9200 # If port 80 leave blank
-# ELASTICSEARCH_USERNAME = ''
-# ELASTICSEARCH_PASSWORD = ''
-# ELASTICSEARCH_INDEX = 'scrapy'
-# ELASTICSEARCH_TYPE = 'items'
-# ELASTICSEARCH_UNIQ_KEY = 'url'
